73817fabad681688b12d16ab153a1cdee9f5804a
Nasser Lamei <nasserlamei21@gmail.com>
2023-07-22 10:52:28 +0300

    first commit

---
+++ MEdiaFinder 2/Models/ActivityIndicator.swift
@@ -0,0 +1,36 @@
+//
+//  ActivityIndicator.swift
+//  MEdiaFinder 2
+//
+//  Created by nassermac on 7/22/23.
+//  Copyright © 2023 Nasser Co. All rights reserved.
+//
+
+import UIKit
+
+class activtyIndicator{
+    
+    static func showActivtyIndicator(view:UIView)->UIActivityIndicatorView{
+        let activty1 = UIActivityIndicatorView(style: .whiteLarge)
+        DispatchQueue.main.async {
+            activty1.center = view.center
+            activty1.color = UIColor.red
+            activty1.startAnimating()
+            view.addSubview(activty1)
+            
+        }
+        return activty1
+    }
+    
+    static func stopActivtyIndicator(activty:UIActivityIndicatorView){
+        DispatchQueue.main.async {
+            activty.stopAnimating()
+            activty.removeFromSuperview()
+        }
+    }
+    
+    
+    
+    
+    
+}
